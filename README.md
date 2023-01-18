5a22174390d141c257c8905ccde6e97ea3579a74
AkarshanMobileFirst <akarshan.mobilefirst@gmail.com>
2023-01-18 14:29:13 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# portfolio_app_akarshan
+ 
---
+++ android/app/src/main/kotlin/com/example/portfolio_app_akarshan/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.portfolio_app_akarshan
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
